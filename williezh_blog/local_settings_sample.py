5a4602d7c304f74a6e71757cefc3ce4301d50a9a
williezh <276971837@qq.com>
2017-03-30 17:12:59 -0400

    migrate db to mysql

--- williezh_blog/local_settings_sample.py
+++ williezh_blog/local_settings_sample.py
@@ -8,7 +8,11 @@ DATABASES = {
         'PASSWORD': '100200',   #密码
         'HOST': '127.0.0.1',
         'PORT': 3306,
-        }
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset':'utf8mb4',
+            },
+        }        
     }
  
 email_conf={
